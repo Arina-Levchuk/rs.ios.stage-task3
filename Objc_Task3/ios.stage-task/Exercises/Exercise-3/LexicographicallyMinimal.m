e6a189ddaf229fd5aae8e9a96c14ceba06dd179c
Arina Levchuk <>
2024-10-17 12:56:22 +0200

    [T3]  ex3 - LexicographicallyMinimal

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -1,4 +1,5 @@
 #import "LexicographicallyMinimal.h"
+#import "NSMutableArray+QueueLinear.h"
 
 @interface LexicographicallyMinimal()
 
@@ -9,7 +10,37 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSMutableArray *charsFromString1Array = [NSMutableArray arrayWithCapacity:string1.length];
+    NSMutableArray *charsFromString2Array = [NSMutableArray arrayWithCapacity:string2.length];
+    NSMutableString *resultString = [NSMutableString string];
+    
+    for (int i = 0; i < string1.length; i++) {
+        [charsFromString1Array addObject:[NSString stringWithFormat:@"%C", [string1 characterAtIndex:i]]];
+    }
+    
+    for (int i = 0; i < string2.length; i++) {
+        [charsFromString2Array addObject:[NSString stringWithFormat:@"%C", [string2 characterAtIndex:i]]];
+    }
+    
+    while (charsFromString1Array.count > 0 && charsFromString2Array.count > 0) {
+        NSComparisonResult comparisonResult = [charsFromString1Array[0] compare:charsFromString2Array[0]];
+        
+        NSString *letter = (comparisonResult == NSOrderedAscending || comparisonResult == NSOrderedSame) ?
+        [charsFromString1Array removeAndReturnFirstObject] :
+        [charsFromString2Array removeAndReturnFirstObject];
+        
+        [resultString appendFormat:@"%@", letter];
+        
+        NSLog(@"%@", resultString);
+    }
+    
+    if (charsFromString1Array.count > 0) {
+        [resultString appendFormat:@"%@", [charsFromString1Array componentsJoinedByString:@""]];
+    } else if (charsFromString2Array.count > 0) {
+        [resultString appendFormat:@"%@", [charsFromString2Array componentsJoinedByString:@""]];
+    }
+    
+    return resultString;
 }
 
 @end
