75ca1fac06cf52fdf6ec3684b78cdedf0a20a6d1
Arina Levchuk <arina.levchuk@innowise.com>
2024-10-22 22:01:36 +0200

    [T3] ex4 in progress

--- Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
@@ -1,6 +1,46 @@
 #import "LevelOrderTraversal.h"
 #import "NSMutableArray+QueueLinear.h"
+#import "TreeNode.h"
 
 NSArray *LevelOrderTraversalForTree(NSArray *tree) {
-    NSMutableArray *levelOrderArray = [NSMutableArray array];
+    NSMutableArray *levelOrderTraversalResult = [NSMutableArray array];
+    
+    if (!tree || tree.count == 0 || tree[0] == [NSNull null]) {
+        return levelOrderTraversalResult;
+    }
+    
+    // create Tree
+    NSMutableArray *nodesTree = [NSMutableArray array];
+    
+    TreeNode *root = [[TreeNode alloc] initWithValue:tree[0]];
+    [nodesTree addObject:root];
+    
+    TreeNode *currentNode = [nodesTree removeAndReturnFirstObject];
+    int index = 1;
+    while (index < tree.count) {
+        if (index < tree.count && tree[index] != [NSNull null]) {
+            currentNode.left = [[TreeNode alloc] initWithValue:tree[index]];
+            [nodesTree addObject:currentNode.left];
+        }
+        index++;
+
+        if (index < tree.count && tree[index] != [NSNull null]) {
+                currentNode.right = [[TreeNode alloc] initWithValue:tree[index]];
+                [nodesTree addObject:currentNode.right];
+        }
+        index++;
+        
+        if ([nodesTree removeAndReturnFirstObject]) {
+            currentNode = [nodesTree removeAndReturnFirstObject];
+        }
+    }
+    [nodesTree addObject:root];
+    
+    
+    
+    for (id node in tree) {
+        //        TreeNode *node =
+    }
+    
+    return levelOrderTraversalResult;
 }
---
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-4/TreeNode.m
@@ -0,0 +1,23 @@
+//
+//  TreeNode.m
+//  ios.stage-task
+//
+//  Created by Arina Levchuk on 22/10/2024.
+//
+
+#import "TreeNode.h"
+
+@implementation TreeNode
+
+- (instancetype)initWithValue:(NSNumber *)value {
+    self = [super init];
+    if (self) {
+        _value = value;
+        _left = nil;
+        _right = nil;
+    }
+    
+    return self;
+}
+
+@end
